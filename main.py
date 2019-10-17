fdb9fb1b4f05bf76ee8b103ed5402540148f6468
Aaron Lockhart <aaronlockhartdev@gmail.com>
2019-10-17 08:19:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,6 @@ import zipfile
 import threading
 import time
 
-def thread_func(f, zip_directory, unzip_directory, num):
-    print(str("{} started unzipping " + f).format("Thread" + str(num)))
-    unzipper = zipfile.ZipFile(zip_directory + '/' + f, 'r')
-    unzipper.extractall(path=unzip_directory)
-    print(str("{} finished unzipping " + f).format("Thread" + str(num)))
 
 def unzip(zip_directory, unzip_directory):
 
@@ -15,14 +10,11 @@ def unzip(zip_directory, unzip_directory):
     threads = list()
 
     for ind, f in enumerate(files):
-        thread = threading.Thread(target=thread_func,
-                                  args=(f, zip_directory, unzip_directory, ind + 1))
-        threads.append(thread)
-        thread.start()
-    print(str(len(threads)) + " started successfully")
-
-    for t in threads:
-        t.join()
+        print(str(ind/len(files) * 100) + "%")
+        
+        unzipper = zipfile.ZipFile(zip_directory + '/' + f, 'r')
+        unzipper.extractall(path=unzip_directory)
+    move_files(unzip_directory)
 
 
 def move_files(unzip_directory):
@@ -33,23 +25,19 @@ def move_files(unzip_directory):
         for f in files:
             try:
                 print(f, end='\r', flush=True)
-                os.rename(directory + '/' + f, 'tmp/' + f)
+                os.rename(directory + '/' + f, 'D:/tmp/' + f)
             except:
                 continue
         os.rmdir(directory)
-    files = os.listdir('tmp')
+    files = os.listdir('D:/tmp')
     for f in files:
-        os.rename('tmp/' + f, unzip_directory + '/' + f)
+        os.rename('D:/tmp/' + f, unzip_directory + '/' + f)
 
 
 def main():
-
-    zip_directory = 'C:/Users/aaron/Downloads/2017 LDS 194 - 200'
-    unzip_directory = 'C:/Users/aaron/Downloads/2017 LDS 194 - 200 - Unzipped'
+    zip_directory = 'D:/LDS_ALL'
+    unzip_directory = 'D:/LDS_EXTRACTED'
     unzip(zip_directory, unzip_directory)
-    move_files(unzip_directory)
-
-
 
 if __name__ == '__main__':
     main()
